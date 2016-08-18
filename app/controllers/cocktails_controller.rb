b5c786a30fccd72aaeacd1ca8f6d645987892088
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-18 11:20:30 +0100

    index page

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,11 +1,14 @@
 class CocktailsController < ApplicationController
   def index
+    @cocktails = Cocktail.all
   end
 
   def show
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
 
   def create
