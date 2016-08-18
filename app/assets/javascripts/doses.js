9372e62cc7d4a0a82f7f188efe0f8e73c6081210
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-18 15:44:47 +0100

    even more done

--- app/assets/javascripts/doses.js
+++ app/assets/javascripts/doses.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function() {
+  $('#new-dose').click(function() {
+    if ($('#dose-form').hasClass('hidden')) {
+    $('#dose-form').removeClass('hidden');
+    } else {
+    $('#dose-form').addClass('hidden');
+    }
+  });
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'cocktails#index'
   resources :cocktails, only: [:index, :show, :new, :create] do
-    resources :doses, only: [:new, :create, :destroy]
+    resources :doses, only: [:create, :destroy]
   end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
