28407e9da31b17ef6e724591f9f2480b2a004d42
Luigi Grazioli <luigi.grazioli.92@gmail.com>
2016-08-19 15:04:47 +0100

    actually done

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,6 +1,7 @@
 class Cocktail < ApplicationRecord
   mount_uploader :photo, PhotoUploader
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
   has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
